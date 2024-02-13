413a689a0a867c45d2d91c54d5cd6ab25cc7d52d
Renato <renatomarquesteles@gmail.com>
2024-02-13 18:56:48 -0300

    feat: adds product screen

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,12 +1,56 @@
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
+import { Feather } from '@expo/vector-icons';
+
+import { PRODUCTS } from '@/utils/data/products';
+import { formatCurrency } from '@/utils/format-currency';
+import { Button } from '@/components/button';
+import { LinkButton } from '@/components/link-button';
 
 export default function Product() {
   const { id } = useLocalSearchParams();
 
+  const product = PRODUCTS.find((item) => item.id === id);
+
+  if (!product) return null;
+
   return (
     <View className="flex-1">
-      <Text>{id}</Text>
+      <Image
+        source={product.cover}
+        className="w-full h-52"
+        resizeMode="cover"
+      />
+
+      <View className="p-5 mt-8 flex-1">
+        <Text className="text-lime-400 text-2xl font-heading my-2">
+          {formatCurrency(product.price)}
+        </Text>
+
+        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+          {product.description}
+        </Text>
+
+        {product.ingredients.map((ingredient) => (
+          <Text
+            key={ingredient}
+            className="text-slate-400 font-body leading-6 text-base"
+          >
+            {'\u2022'} {ingredient}
+          </Text>
+        ))}
+      </View>
+
+      <View className="p-5 pb-8 gap-5">
+        <Button>
+          <Button.Icon>
+            <Feather name="plus-circle" size={20} />
+          </Button.Icon>
+          <Button.Text>Add to cart</Button.Text>
+        </Button>
+
+        <LinkButton title="Back to Menu" href="/" />
+      </View>
     </View>
   );
 }
